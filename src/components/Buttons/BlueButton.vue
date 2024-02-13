3302807ecad9af5b968bf47fd2f76ba6dfcabdf3
valenthyne-ut <valenthyne.ut@gmail.com>
2024-02-13 17:24:17 +0000

    Change colored button text color to white

--- src/components/Buttons/BlueButton.vue
+++ src/components/Buttons/BlueButton.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-	<button type="button" class="px-2 py-1 rounded transition-colors bg-blue-600 hover:bg-blue-700 active:bg-blue-800">
+	<button type="button" class="px-2 py-1 rounded transition-colors bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white">
 		<slot></slot>
 	</button>
 </template>
