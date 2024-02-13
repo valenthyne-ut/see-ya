ee620b9643f36abce97e54cb371b4437a891a057
valenthyne-ut <valenthyne.ut@gmail.com>
2024-02-13 13:40:31 +0000

    Make upload buttons more accessible

--- src/components/RootView/ClipboardUpload.vue
+++ src/components/RootView/ClipboardUpload.vue
@@ -2,10 +2,13 @@
 </script>
 
 <template>
-	<button type="button"
-		class="w-16 h-16 rounded-lg flex justify-center items-center
-		transition-colors cursor-pointer
-		text-white bg-blue-600 hover:bg-blue-700 active:bg-blue-800">
-		<span class="bi-clipboard2-fill text-3xl"></span>
+	<div class="flex flex-col items-center">
+		<button type="button" id="clipboardUploadButton"
+			class="w-16 h-16 rounded-lg flex justify-center items-center
+			transition-colors cursor-pointer
+			text-white bg-blue-600 hover:bg-blue-700 active:bg-blue-800">
+			<span class="bi-clipboard2-fill text-3xl"></span>
 		</button>
+		<label for="clipboardUploadButton" class="mt-2 text-sm">the text.</label>
+	</div>
 </template>
--- src/components/RootView/MessageUpload.vue
+++ src/components/RootView/MessageUpload.vue
@@ -2,11 +2,14 @@
 </script>
 
 <template>
-	<label for="messageUpload"
-		class="w-16 h-16 rounded-lg flex justify-center items-center
-		transition-colors cursor-pointer
-		text-white bg-green-500 hover:bg-green-600 active:bg-green-700">
-		<span class="bi-upload text-3xl"></span>
-	</label>
-	<input id="messageUpload" type="file" class="hidden" />
+	<div class="flex flex-col items-center">
+		<label for="messageUpload" role="button" id="messageUploadButton" tabindex="0"
+			class="w-16 h-16 rounded-lg flex justify-center items-center
+			transition-colors cursor-pointer
+			text-white bg-green-500 hover:bg-green-600 active:bg-green-700">
+			<span class="bi-upload text-3xl"></span>
+		</label>
+		<input id="messageUpload" type="file" class="hidden" />
+		<label for="messageUploadButton" class="mt-2 text-sm">the file.</label>
+	</div>
 </template>
